2b4525ba9706d63684d02c63be1fa3b6ba63bc46
IsaiahSama <zeldaplayergl@gmail.com>
2021-09-22 17:18:03 -0400

    Added the ban command

--- admin.py
+++ admin.py
@@ -18,6 +18,17 @@ class Admin(Cog):
         await member.kick(reason=reason)
         await ctx.send(f"{member} has been kicked from the server by {ctx.author}. Reason: {reason}")
 
+    @command(name="Ban", brief="Bans a user from the server", help="Used to ban a user from the server", usage="@user|user_id reason")
+    @has_guild_permissions(ban_members=True)
+    async def ban(self, ctx:Context, user:discord.User, reason:str):
+        try:
+            await user.send(f"You have been banned from PCSG. Reason: {reason}")
+        except:
+            pass 
+        await ctx.guild.ban(user)
+        await ctx.send(f"{user} has been banned by {ctx.author}. Reason: {reason}")
+
+
 def setup(bot: Bot):
     bot.add_cog(Admin(bot))
     
