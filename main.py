8d5907c3b315346f398e2aed4bc75d0ac63f93b0
IsaiahSama <zeldaplayergl@gmail.com>
2021-09-24 21:22:44 -0400

    Added slash commands

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from discord import Intents
 from discord.ext.commands import Bot
+from discord_slash import SlashCommand
 from config import config
 from os import listdir
 
 bot = Bot(command_prefix=config['constants']['prefix'], intents=Intents.all())
+slash = SlashCommand(bot, sync_commands=True, sync_on_cog_reload=True)
 
 # Loads extensions
 [bot.load_extension(file.strip(".py")) for file in listdir() if file.endswith(".py") and file not in config['non_cogs']]
