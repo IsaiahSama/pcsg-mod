dbb9746ae48aa625ec5afcbff346d1fb778bf26d
IsaiahSama <zeldaplayergl@gmail.com>
2021-10-15 15:06:06 -0400

    Added command to select learning style

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from discord.channel import TextChannel
 from discord import Embed, Member
 from discord.ext.commands import Bot, Cog
 from discord_slash import SlashContext, cog_ext
-from discord_slash.utils.manage_commands import create_option
+from discord_slash.utils.manage_commands import create_choice, create_option
 from config import config
 from database import db
 from random import randint
@@ -42,6 +42,31 @@ class Utils(Cog):
             embed.add_field(name="Name:", value=ctx.guild.get_member(user[0]) or "Unknown User")
             embed.add_field(name="Exp:", value=user[1])
         await ctx.send(embed=embed)
+
+    @cog_ext.cog_slash(name="style", description="Used to view and choose your learning style", guild_ids=config['guild_ids'],
+    options=[
+        create_option("learn", "Select a learning style that you want to learn more about", str, False, choices=[create_choice(value, name) for value, name in zip(config['styles'].keys(), [name.split("\n")[0] for name in config['styles'].values()])]),
+        create_option("choose", "Select your learning style from the provided options", str, False, choices=[create_choice(value, name) for value, name in zip(config['styles'].keys(), [name.split("\n")[0] for name in config['styles'].values()])]),
+        create_option("find", "Finds other students that have a similar learning style to you", str, False, choices=[create_choice(value, name) for value, name in zip(config['styles'].keys(), [name.split("\n")[0] for name in config['styles'].values()])])
+    ])
+    async def style(self, ctx:SlashContext, learn:str='', choose:str='', find:str=''):
+        # await ctx.defer()
+        if not any((find, learn, choose)): await ctx.send("Nothing was selected", delete_after=5); return False 
+        if learn:
+            await ctx.send(config['styles'][learn])
+        elif choose:
+            try:
+                role = config['role_styles'][choose]
+                await ctx.author.add_roles(role) if role not in ctx.author.roles else ctx.author.remove_roles(role)
+                await ctx.send(f"You have successfully {'gained' if role in ctx.author.roles else 'removed'} the {role.name} role")
+            except KeyError:
+                await ctx.send("Sorry, we're still waiting on the roles to be created before this can work.")
+        else:
+            try:
+                role = config['role_styles'][choose]
+            except KeyError:
+                await ctx.send("Sorry, we're still waiting on the roles to be created before this can work.")
+
 
 def setup(bot:Bot):
     bot.add_cog(Utils(bot))
